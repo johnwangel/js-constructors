e60082d18af98b5479a0aacc4be9097d48c72f81
John Atkins <johnatkins1999@yahoo.com>
2017-06-25 11:00:41 -1000

    dev

--- constructors.js
+++ constructors.js
@@ -11,7 +11,7 @@
  * @method   getDetails
  */
 
-  /**
+   /**
    * Returns a string of all of the spell's details.
    * The format doesn't matter, as long as it contains the spell name, cost, and description.
    *
@@ -44,6 +44,7 @@
  * @property {string} description
  */
 
+
 /**
  * Now that you've created some spells, let's create
  * `Spellcaster` objects that can use them!
@@ -61,16 +62,16 @@
  * @method  invoke
  */
 
-  /**
-   * @method inflictDamage
-   *
-   * The spellcaster loses health equal to `damage`.
-   * Health should never be negative.
-   * If the spellcaster's health drops to 0,
-   * its `isAlive` property should be set to `false`.
-   *
-   * @param  {number} damage  Amount of damage to deal to the spellcaster
-   */
+ /**
+ * @method inflictDamage
+ *
+ * The spellcaster loses health equal to `damage`.
+ * Health should never be negative.
+ * If the spellcaster's health drops to 0,
+ * its `isAlive` property should be set to `false`.
+ *
+ * @param  {number} damage  Amount of damage to deal to the spellcaster
+ */
 
   /**
    * @method spendMana
@@ -81,6 +82,78 @@
    * @param  {number} cost      The amount of mana to spend.
    * @return {boolean} success  Whether mana was successfully spent.
    */
+
+function Spell(name, cost, description){
+    this.name = name;
+    this.cost = cost;
+    this.description = description;
+}
+
+Spell.prototype.getDetails = function() {
+    return  "Name: " + this.name + ", Cost: " + this.cost + ", Description: " + this.description;
+};
+
+DamageSpell.prototype = Object.create(Spell.prototype);
+
+function DamageSpell(name, cost, damage, description){
+  Spell.call(this, name, cost, description);
+  this.damage = damage;
+}
+
+function Spellcaster(name, health, mana){
+  this.name = name;
+  this.health = health;
+  this.mana = mana;
+  this.isAlive = true;
+ }
+
+ Spellcaster.prototype.inflictDamage = function(damage){
+  if (this.health - damage < 0) {
+    this.health = 0;
+  } else {
+    this.health -= damage;
+    console.log(this.name + " was dealt " + damage + " damage and now has " + this.health + " health");
+  }
+  if (this.health <= 0) {
+    this.isAlive = false;
+  }
+ };
+
+ Spellcaster.prototype.spendMana = function(cost){
+  if (this.mana - cost >= 0 ){
+    console.log ("SPENDING: " + this.name + " started with mana " + this. mana);
+    this.mana -= cost;
+    console.log("NOW has mana " + this.mana);
+    return true;
+  } else {
+    console.log("SPENDING: " + this.name + " has " + this.mana + " and is trying to buy a spell costing " + cost);
+    return false;
+  }
+ };
+
+ Spellcaster.prototype.invoke = function(spell, target){
+    if (spell == undefined || spell.name === undefined || spell.cost === undefined ) { return false; }
+    if (spell instanceof Spell && spell !== null && !(spell instanceof DamageSpell) && (spell.cost > 0) && this.mana >= spell.cost){
+        console.log("NON-DAMAGE-SPELL: " + spell.name + " is a spell");
+        console.log(this.name + " is invoking " + spell.name + " which costs " + spell.cost);
+        this.spendMana(spell.cost);
+        console.log(this.name + " invoked " + spell.name + " and now has " + this.mana + " mana");
+        return true;
+    } else if (spell !== null && spell instanceof DamageSpell && (target) && (spell.cost !== undefined) && this.mana >= spell.cost && target.isAlive) {
+        console.log(spell.name + " is a damage spell");
+        console.log(this.name + " has " + this.mana + " and is trying to buy a spell that is " + spell.cost + ". ");
+        console.log("Trying to inflict damage on " + target.name + " while isAlive is " + target.isAlive + " and he has health of " + target.health + " and is being inflicted damage of " + spell.damage);
+        this.spendMana(spell.cost);
+        console.log("Now " + this.name + " has " + this.mana + " mana");
+        this.inflictDamage.call(target, spell.damage);
+        console.log("Now " + target.name + " has " + target.health + " health");
+        return true;
+    } else if (spell.cost !== undefined) {
+        console.log(this.name + " has " + this.mana + " and is trying to buy a spell that is " + spell.cost + ". ");
+        return false;
+    }
+    return false;
+ };
 
   /**
    * @method invoke
